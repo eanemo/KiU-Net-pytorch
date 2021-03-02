15f6f590ec015e5c6844fcfa5858ebec1111fb6b
eanemo <eaguilera@nemotec.org>
2021-03-02 14:14:13 +0100

    Modified some code

--- convert_dataset.py
+++ convert_dataset.py
@@ -10,7 +10,7 @@ files = glob.glon("*.png")
 
 for file in files:
     img = cv2.imread(join(input_dir, file))
-    out_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    out_img = cv2.resize(out_img, (128, 128))
+    out_img = cv2.resize(img, (128, 128))
+    out_img = cv2.cvtColor(out_img, cv2.COLOR_BGR2GRAY)
     cv2.imwrite(join(output_dir, file), out_img)
     print("Writing ", join(output_dir, file))
