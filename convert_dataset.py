9786f9741e2abf8bbf3601c6af301868ed8c3965
eanemo <eaguilera@nemotec.org>
2021-03-03 08:19:01 +0100

    Modified some code

--- convert_dataset.py
+++ convert_dataset.py
@@ -13,5 +13,5 @@ for file in files:
     img = cv2.imread(join(input_dir, file))
     out_img = cv2.resize(img, (128, 128))
     out_img = cv2.cvtColor(out_img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(join(output_dir, file), out_img)
+    cv2.imwrite(join(output_dir, file), out_img[:, :, 1])
     print("Writing ", join(output_dir, file))
